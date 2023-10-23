8d69bcc1d4ca734d0a506106ec24b3b091e82ae9
Vidal Herrero Garcia <104066977+GaldosV@users.noreply.github.com>
2023-10-23 14:24:46 +0200

    Creación de deepl
    Creación preeliminar del envio de datoa a deep l

---
+++ ScraperNoticias.py
@@ -0,0 +1,107 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+def obtener_enlaces_noticias(url):
+    enlaces = []
+    
+    # Realiza una solicitud HTTP para obtener la página
+    response = requests.get(url)
+    
+    # Comprueba si la solicitud fue exitosa
+    if response.status_code == 200:
+        # Parsea el contenido de la página web con BeautifulSoup
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        # Encuentra el elemento <section id="24 hours">
+        section = soup.find('section', {'id': '24Hours'})
+        
+        if section:
+            # Encuentra el elemento <div class="posts">
+            posts = section.find('div', {'class': 'posts'})
+            
+            if posts:
+                # Encuentra todos los enlaces <a href="">
+                links = posts.find_all('a', href=True)
+                
+                for link in links[:10]:  # Obtén los enlaces de las 10 primeras noticias
+                    enlace = link['href']
+                    enlaces.append(enlace)
+                    #Imprime por consola los enlaces 
+                    print (enlace)
+        else:
+            print('No se pudo encontrar el elemento <section id="24Hours"> en la página.')
+    else:
+        print('No se puede cargar la pagina .')
+    
+    return enlaces
+
+def scrape_noticia(url):
+    try:
+        # Realiza una solicitud HTTP para obtener la página
+        response = requests.get(url)
+
+        # Comprueba si la solicitud fue exitosa
+        if response.status_code != 200:
+            print(f'No se pudo acceder a la página: {url}')
+            return
+
+        # Parsea el contenido de la página web con BeautifulSoup
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Extrae el título de la noticia (si está disponible)
+        title = soup.find('h1', class_='post-title')
+        if title:
+            title = title.get_text()
+        else:
+            title = 'Título no encontrado'
+
+        # Encuentra el elemento <div class="post-box clearfix">
+        post_box = soup.find('div', class_='post-box clearfix')
+
+        if post_box:
+            # Encuentra el elemento <article> dentro de post_box
+            article = post_box.find('article')
+
+            if article:
+                # Excluye el contenido de no deseado (un disclaimer y la bio del autor)
+                for unwanted in article.find_all(['div', 'span'], class_=['post-meta-flex', 'footer-disclaimer']):
+                    unwanted.extract()
+
+                # Extrae el contenido del artículo
+                article_content = ''
+                paragraphs = article.find_all('p')
+                for p in paragraphs:
+                    article_content += p.get_text() + '\n'
+
+                # Extrae la fecha de la noticia (si está disponible)
+                article_Date = soup.find('div', class_='post-date')
+                if article_Date:
+                    article_Date = article_Date.get_text()
+                else:
+                    article_Date = 'Fecha no encontrada'
+
+                # Link de la noticia (el mismo que el proporcionado)
+                article_Link = url
+
+                # Crea la ruta al archivo de texto en la carpeta "Noticias"
+                carpeta_noticias = os.path.join(os.path.dirname(__file__), 'Noticias')
+                os.makedirs(carpeta_noticias, exist_ok=True)
+                nombre_archivo = os.path.join(carpeta_noticias, f'noticia_{article_Link.replace("https://", "").replace("/", "_")}.txt')
+
+                # Abre el archivo en modo escritura y escribe los datos
+                with open(nombre_archivo, 'w', encoding='utf-8') as archivo:
+                    archivo.write('Título de la noticia: ' + title + '\n')
+                    archivo.write('Contenido de la noticia: ' + article_content + '\n')
+                    archivo.write('Fecha de la Noticia: ' + article_Date + '\n')
+                    archivo.write('Link de la Noticia: ' + article_Link + '\n')
+
+                print('Los datos se han guardado en', nombre_archivo)
+            else:
+                print('No se pudo encontrar el elemento <article> en la página.')
+        else:
+            print('No se pudo encontrar el elemento <div class="post-box clearfix"> en la página.')
+
+    except Exception as e:
+        print(f'Ocurrió un error: {e}')
+
---
+++ TraducirNoticias.py
@@ -0,0 +1,42 @@
+import requests
+
+def traducir_texto(texto_ingles, clave_api):
+    # URL de la API de DeepL 
+    url = "https://api.deepl.com/v2/translate"
+
+    # Configura los parámetros de la solicitud POST
+    params = {
+        "auth_key": clave_api,
+        "text": texto_ingles,
+        "source_lang": "EN",  # Idioma de origen (inglés)
+        "target_lang": "ES",  # Idioma de destino (español)
+    }
+
+    # Envía la solicitud POST 
+    response = requests.post(url, data=params)
+
+    # Analiza la respuesta JSON
+    data = response.json()
+
+    # Obtiene el texto traducido
+    texto_traducido = data["translations"][0]["text"]
+
+    return texto_traducido
+
+
+# Pruebas:
+
+# Lee el contenido del archivo de texto
+archivo_txt = "Noticias/1.txt"
+
+with open(archivo_txt, "r", encoding="utf-8") as file:
+    texto_generado_en_ingles = file.read()
+
+clave_de_api = "2f0cf10d-ff5b-3f6c-943f-4bb71deb286b"  # Reemplaza con tu clave de API real 2f0cf10d-ff5b-3f6c-943f-4bb71deb286b
+
+texto_traducido = traducir_texto(texto_generado_en_ingles, clave_de_api)
+
+print("Texto original en inglés:")
+print(texto_generado_en_ingles)
+print("Texto traducido al español:")
+print(texto_traducido)
